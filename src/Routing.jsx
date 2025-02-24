f2a5f24b37551272a0f5aed2b27362a806621ce7
fady <fastfady2019@gmail.com>
2025-02-24 20:54:21 +0200

    fix productDetails

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import AllProducts from './components/sharedComponents/Allproducts';
-import CategoryProducts from './components/sharedComponents/CategoryProducts';
+import CategoryProducts from './components/sharedComponents/CategoryProducts'
+import SingleProduct from './components/sharedComponents/SingleProduct';
 
 export const Routing = () => {
 	return (
@@ -9,7 +10,8 @@ export const Routing = () => {
 			<Route path="/signup" element={<h1>Sign Up</h1>} />
 			<Route path="/" element={<h1>Home</h1>} />
 			<Route path="/allproducts" element={<><AllProducts/></>} />
-			<Route path="/CategoryProducts" element={<><CategoryProducts caterg/></>} />
+			<Route path="/product/:id" element={<SingleProduct />} />
+			<Route path="/CategoryProducts" element={<><CategoryProducts/></>} />
 			<Route path="/about" element={<h1>About</h1>} />
 			<Route path="/contact" element={<h1>Contact</h1>} />
 			<Route path="*" element={<h1>Error Page</h1>} />
